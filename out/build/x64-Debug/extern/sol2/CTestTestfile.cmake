1860132a7d8e18e4f3fd65fa8eefd98068922756
Rahul Kumar Mahato <rkrm2405@gmail.com>
2025-10-25 14:03:07 +0530

    Fix: Remove .vs and build/ folders from Git tracking

--- out/build/x64-Debug/extern/sol2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/NovaEngine/extern/sol2
-# Build directory: D:/Projects/NovaEngine/out/build/x64-Debug/extern/sol2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
